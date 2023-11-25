02d52d487aae53a157a1d0de75dda8492171baf4
geekqq <jinwei1014@gmail.com>
2023-11-24 17:42:45 -0800

    new codes

---
+++ arithmeticoperators.py
@@ -0,0 +1,66 @@
+# Arithmetic operator
+x = 2
+y = 7
+
+total = x + y
+print(total)
+
+total = x - y
+print(total)
+
+total = x * y
+print(total)
+
+total = y / x
+print(total)
+
+total = y % x
+print(total)
+
+total = y ** x
+print(total)
+
+
+# Comparison Operator
+
+a = 30
+b = 60
+
+out = a < b
+print(out)
+
+out = a > b
+print(out)
+
+out = (a != b)
+print(out)
+
+out = (a >= b)
+print(out)
+
+out = (a <= b)
+print(out)
+
+# Assignment Operator
+c = 0
+d = 1
+
+c += d
+print(c)
+
+# Logical Operator
+# and or
+a = 40
+b = 60
+x = 2
+y = 3
+
+out = (a < b) or (x > y)
+print(out)
+
+a = 11
+b = 12
+res = a is b
+print(res)
+res = a is not b
+print(res)
